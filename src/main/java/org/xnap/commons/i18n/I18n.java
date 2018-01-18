b8ef2b002d277b4df2570bf74d4ac843cee559bc
Franco Vitali <francovitali@gmail.com>
2018-01-18 12:38:45 -0300

    added trc overload

--- src/main/java/org/xnap/commons/i18n/I18n.java
+++ src/main/java/org/xnap/commons/i18n/I18n.java
@@ -33,7 +33,7 @@ import java.util.ResourceBundle;
  * To learn how message strings wrapped in one of the <code>tr*()</code>
  * methods can be extracted and localized, see <a
  * href="http://code.google.com/p/gettext-commons/wiki/Tutorial">this tutorial</a>.
- * 
+ *
  * @author Steffen Pingel
  * @author Felix Berger
  * @author Tammo van Lessen
@@ -42,7 +42,7 @@ import java.util.ResourceBundle;
 public class I18n {
 
 	private static final String CONTEXT_GLUE = "\u0004";
-	
+
 	/**
 	 * Reference to the current localization bundles.
 	 */
@@ -50,7 +50,7 @@ public class I18n {
 
 	/**
 	 * The locale of the strings used in the source code.
-	 * 
+	 *
 	 * @see #trc(String, String)
 	 */
 	private volatile Locale sourceCodeLocale = Locale.ENGLISH;
@@ -63,7 +63,7 @@ public class I18n {
 
 	/**
 	 * Constructs an I18n object for a resource bundle.
-	 * 
+	 *
 	 * @param bundle
 	 *            must not be <code>null</code>
 	 * @throws NullPointerException
@@ -78,7 +78,7 @@ public class I18n {
 	/**
 	 * Constructs an I18n object by calling {@link #setResources(String, Locale,
 	 * ClassLoader)}.
-	 * 
+	 *
 	 * @throws MissingResourceException
 	 *             if the resource bundle could not be loaded
 	 * @throws NullPointerException
@@ -92,7 +92,7 @@ public class I18n {
 
 	/**
 	 * Returns the current resource bundle.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public ResourceBundle getResources()
@@ -104,7 +104,7 @@ public class I18n {
 	 * Returns the locale this instance was created with. This can be different
 	 * from the locale of the resource bundle returned by
 	 * {@link #getResources()}.
-	 * 
+	 *
 	 * @return the locale or null, if this instance was directly created from a
 	 *         resource bundle
 	 * @since 0.9
@@ -121,7 +121,7 @@ public class I18n {
 	 * baseName are invalidated, since the bundle might be from a different
 	 * context. Subsequent calls to {@link #setLocale(Locale)} won't have any
 	 * effect.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public synchronized void setResources(ResourceBundle bundle)
@@ -139,7 +139,7 @@ public class I18n {
 	 * Tries to load a resource bundle using {@link
 	 * ResourceBundle#getBundle(java.lang.String, java.util.Locale,
 	 * java.lang.ClassLoader)}.
-	 * 
+	 *
 	 * @throws MissingResourceException
 	 *             if the bundle could not be loaded
 	 * @throws NullPointerException
@@ -163,7 +163,7 @@ public class I18n {
 	/**
 	 * Marks <code>text</code> to be translated, but doesn't return the
 	 * translation but <code>text</code> itself.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public static final String marktr(String text)
@@ -178,7 +178,7 @@ public class I18n {
 	 * to retrieve all messages anew after a locale change in order for them to
 	 * be translated to the language specified by the new locale.
 	 * <p>
-	 * 
+	 *
 	 * @return false if there is not enough information for loading a new
 	 *         resource bundle, see {@link #setResources(ResourceBundle)}.
 	 * @throws MissingResourceException
@@ -206,7 +206,7 @@ public class I18n {
 	 * Only languages that have one singular and one plural form can be used as
 	 * source code locales, since {@link #trn(String, String, long)} takes
 	 * exactly these two forms as parameters.
-	 * 
+	 *
 	 * @param locale
 	 *            the locale
 	 * @throws NullPointerException
@@ -226,7 +226,7 @@ public class I18n {
 	 * Returns <code>text</code> translated into the currently selected
 	 * language. Every user-visible string in the program must be wrapped into
 	 * this function.
-	 * 
+	 *
 	 * @param text
 	 *            text to translate
 	 * @return the translation
@@ -251,7 +251,7 @@ public class I18n {
 	 * <p>
 	 * Invokes
 	 * {@link MessageFormat#format(java.lang.String, java.lang.Object[])}.
-	 * 
+	 *
 	 * @param text
 	 *            text to translate
 	 * @param objects
@@ -266,7 +266,7 @@ public class I18n {
 
 	/**
 	 * Convenience method that invokes {@link #tr(String, Object[])}.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String tr(String text, Object o1)
@@ -276,7 +276,7 @@ public class I18n {
 
 	/**
 	 * Convenience method that invokes {@link #tr(String, Object[])}.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String tr(String text, Object o1, Object o2)
@@ -286,7 +286,7 @@ public class I18n {
 
 	/**
 	 * Convenience method that invokes {@link #tr(String, Object[])}.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String tr(String text, Object o1, Object o2, Object o3)
@@ -296,7 +296,7 @@ public class I18n {
 
 	/**
 	 * Convenience method that invokes {@link #tr(String, Object[])}.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String tr(String text, Object o1, Object o2, Object o3, Object o4)
@@ -307,7 +307,7 @@ public class I18n {
 	/**
 	 * Returns the plural form for <code>n</code> of the translation of
 	 * <code>text</code>.
-	 * 
+	 *
 	 * @param text
 	 *            the key string to be translated.
 	 * @param pluralText
@@ -330,7 +330,7 @@ public class I18n {
 	/**
 	 * Returns the plural form for <code>n</code> of the translation of
 	 * <code>text</code>.
-	 * 
+	 *
 	 * @param text
 	 *            the key string to be translated.
 	 * @param pluralText
@@ -351,7 +351,7 @@ public class I18n {
 	 * Overloaded method that invokes
 	 * {@link #trn(String, String, long, Object[])} passing <code>Object</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String trn(String text, String pluralText, long n, Object o1)
@@ -363,7 +363,7 @@ public class I18n {
 	 * Overloaded method that invokes
 	 * {@link #trn(String, String, long, Object[])} passing <code>Object</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String trn(String text, String pluralText, long n, Object o1, Object o2)
@@ -375,7 +375,7 @@ public class I18n {
 	 * Overloaded method that invokes
 	 * {@link #trn(String, String, long, Object[])} passing <code>Object</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String trn(String text, String pluralText, long n, Object o1, Object o2, Object o3)
@@ -387,7 +387,7 @@ public class I18n {
 	 * Overloaded method that invokes
 	 * {@link #trn(String, String, long, Object[])} passing <code>Object</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9
 	 */
 	public final String trn(String text, String pluralText, long n, Object o1, Object o2, Object o3, Object o4)
@@ -397,10 +397,10 @@ public class I18n {
 
 	/**
 	 * Returns the plural form for <code>n<code> of the translation of ???
-	 *      
+	 *
 	 * Based on GettextResource.java that is part of GNU gettext for Java
 	 * Copyright (C) 2001 Free Software Foundation, Inc.
-	 * 
+	 *
 	 * @param bundle a ResourceBundle
 	 * @param text the key string to be translated, an ASCII string
 	 * @param pluralText its English plural form
@@ -480,7 +480,7 @@ public class I18n {
 
 	/**
 	 * Disambiguates translation keys.
-	 * 
+	 *
 	 * @param context
 	 *            the context of the text to be translated
 	 * @param text
@@ -488,26 +488,47 @@ public class I18n {
 	 * @return <code>text</code> if the locale of the underlying resource
 	 *         bundle equals the source code locale, the disambiguated
 	 *         translation of <code>text</code> otherwise
-	 *         
+	 *
 	 * @see #setSourceCodeLocale(Locale)
 	 * @since 0.9
 	 */
 	public final String trc(String context, String text)
 	{
-		if (sourceCodeLocale.equals(getResources().getLocale())) { 
+		if (sourceCodeLocale.equals(getResources().getLocale())) {
 			return text;
 		} else {
-			String key = context + CONTEXT_GLUE + text; 
+			String key = context + CONTEXT_GLUE + text;
 			String translated = tr(key);
 			// if no translation was found return text in source locale
 			return translated == key ? text : translated;
 		}
 	}
-	
-	/** 
+
+	/**
+	 * Disambiguates translation keys.
+	 *
+	 * @param context
+	 *            the context of the text to be translated
+	 * @param text
+	 *            the ambiguous key message in the source locale
+	 * @param objects
+	 *            object args to be formatted and substituted.
+	 * @return <code>text</code> if the locale of the underlying resource
+	 *         bundle equals the source code locale, the disambiguated
+	 *         translation of <code>text</code> otherwise
+	 *
+	 * @see #setSourceCodeLocale(Locale)
+	 * @since 9.13-ml
+	 */
+	public final String trc(String context, String text, Object[] objects)
+	{
+        return MessageFormat.format(trc(context, text), objects);
+	}
+
+	/**
 	 * Returns the plural form for <code>n</code> of the translation of
 	 * <code>text</code>.
-	 * 
+	 *
 	 * @param context
 	 * 			  the context of the message to disambiguate it when translating
 	 * @param singularText
@@ -531,7 +552,7 @@ public class I18n {
 	/**
 	 * Returns the plural form for <code>n</code> of the translation of
 	 * <code>text</code>.
-	 * 
+	 *
 	 * @param context
 	 * 			  the context of the message to disambiguate it when translating
 	 * @param singularText
@@ -553,44 +574,44 @@ public class I18n {
 	 * Overloaded method that invokes
 	 * {@link #trnc(String, String, String, long, Object[])} passing <code>obj</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9.5
 	 */
 	public final String trnc(String comment, String singularText, String pluralText, long n, Object obj) {
 		return MessageFormat.format(trnc(comment, singularText, pluralText, n), new Object[] { obj });
 	}
-	
+
 	/**
 	 * Overloaded method that invokes
-	 * {@link #trnc(String, String, String, long, Object[])} passing <code>obj1</code> and <code>obj2</code> 
+	 * {@link #trnc(String, String, String, long, Object[])} passing <code>obj1</code> and <code>obj2</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9.5
 	 */
 	public final String trnc(String comment, String singularText, String pluralText, long n, Object obj1, Object obj2) {
 		return MessageFormat.format(trnc(comment, singularText, pluralText, n), new Object[] { obj1, obj2 });
 	}
-	
+
 	/**
 	 * Overloaded method that invokes
 	 * {@link #trnc(String, String, String, long, Object[])} passing <code>obj1</code>, <code>obj2</code> and <code>obj3</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9.5
 	 */
 	public final String trnc(String comment, String singularText, String pluralText, long n, Object obj1, Object obj2, Object obj3) {
 		return MessageFormat.format(trnc(comment, singularText, pluralText, n), new Object[] { obj1, obj2, obj3 });
 	}
-	
+
 	/**
 	 * Overloaded method that invokes
 	 * {@link #trnc(String, String, String, long, Object[])} passing <code>obj1</code>, <code>obj2</code>, <code>obj3</code> and <code>obj4</code>
 	 * arguments as an array.
-	 * 
+	 *
 	 * @since 0.9.5
 	 */
 	public final String trnc(String comment, String singularText, String pluralText, long n, Object obj1, Object obj2, Object obj3, Object obj4) {
 		return MessageFormat.format(trnc(comment, singularText, pluralText, n), new Object[] { obj1, obj2, obj3, obj4 });
 	}
-	
+
 }
